ca17fb2c8ca7a31fda6894ab2168443088acb96f
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-08-29 20:36:23 +0900

    initial commit

---
+++ src/main/java/me/songjaegeun/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,12 @@
+package me.songjaegeun.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
